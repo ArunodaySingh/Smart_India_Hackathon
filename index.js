2b1631752e7f8893c0c319376934bdf248edff7f
ArunodaySingh <arunodaysingh137@gmail.com>
2023-09-27 09:47:43 +0530

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).send("I got the request");
+});
+
+app.post("/", (req, res) => {
+  res.status(200).send("I got the request");
+});
+
 app.listen("3000", (err) => {
   if (!err) console.log("connected");
 });
